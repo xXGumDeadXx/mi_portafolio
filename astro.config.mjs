b9ba21d8d03ac9c73709d010aa4aba40f279af0e
angel <angeljlinarez@gmail.com>
2024-12-19 00:25:40 -0400

    CONFIG

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,17 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+    vite: {
+        ssr: {
+            noExternal: ['webcoreui']
+        },
+        css: {
+          preprocessorOptions: {
+              scss: {
+                  api: 'modern-compiler'
+              }
+          }
+      }
+    }
+})
